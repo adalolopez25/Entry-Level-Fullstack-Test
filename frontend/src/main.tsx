dc878c3ed1432f6ea003bf357d50dabe5c2207d6
adalolopez25 <adalolopez25@gmail.com>
2025-11-10 19:45:41 -0500

    chore: resolve merge conflict in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,9 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-<<<<<<< HEAD
 import "./index.css";
-=======
-import './index.css'
->>>>>>> c23096c38fde6f2ae5af57717070bfe3ac6570be
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
